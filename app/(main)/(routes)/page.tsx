1bb0aa51dafd7042ff1d89ed648467cf7dd2073a
heetvadiya <heetvadiya@gmail.com>
2024-01-31 00:16:50 +0530

    configured database & started with server creation

--- app/(main)/(routes)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserButton } from "@clerk/nextjs"
-export default function Home() {
-  return (
-    <div>
-    <UserButton afterSignOutUrl="/"/>
-    <p className='text-6xl font-bold'>  This is protected route </p>
-    </div>
-  )
-}
